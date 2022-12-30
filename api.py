227177319974d8004d73c55f3dff962316f08203
fi3te <fi3te@users.noreply.github.com>
2022-12-30 18:40:02 +0100

    Fixed authorization bug

--- api.py
+++ api.py
@@ -8,14 +8,14 @@ from model import Data
 
 def _prepared_payload() -> dict:
     return {
-        'token': constants.TOKEN,
         'sync_token': '*',
         'resource_types': '["projects", "items"]'
     }
 
 
 def load() -> Tuple[Optional[Data], dict]:
-    response = requests.post(constants.SYNC_ENDPOINT, _prepared_payload())
+    headers = {'Authorization': f'Bearer {constants.TOKEN}'}
+    response = requests.post(constants.SYNC_ENDPOINT, _prepared_payload(), headers=headers)
     content = response.json()
     try:
         data = Data(**content)
